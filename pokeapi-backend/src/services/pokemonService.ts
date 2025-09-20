16dfa89eae05a51903f162fd8eaeb37bb7eb2a4b
David Olah <59338719+odavid0414@users.noreply.github.com>
2025-09-20 16:17:59 +0200

    fix userid error

--- pokeapi-backend/src/services/pokemonService.ts
+++ pokeapi-backend/src/services/pokemonService.ts
@@ -1,11 +1,11 @@
 import { prisma } from '../../prisma/prismaClient';
 export async function catchPokemon(id: number, name: string, height: number, weight: number, sprite_front_default: string, userId: number) {
-    const pokemon = await prisma.pokemon.create({ data: { id, name, height, weight, sprite_front_default, userId } });
+    const pokemon = await prisma.pokemon.create({ data: { id, name, height, weight, sprite_front_default } });
     return pokemon;
 }
 
 export async function releasePokemon(id: number, userId: number) {
-    const pokemon = await prisma.pokemon.delete({ where: { id, userId } });
+    const pokemon = await prisma.pokemon.delete({ where: { id } });
     return pokemon;
 }
 
