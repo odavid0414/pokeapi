e019e094f028b7ca27cc20695d5ce95e854d4354
David Olah <59338719+odavid0414@users.noreply.github.com>
2025-09-21 16:14:21 +0200

    fix prisma queries

--- pokeapi-backend/src/services/pokemonService.ts
+++ pokeapi-backend/src/services/pokemonService.ts
@@ -1,20 +1,34 @@
 import { prisma } from '../../prisma/prismaClient';
 export async function catchPokemon(id: number, name: string, height: number, weight: number, sprite_front_default: string, userId: number) {
-    const pokemon = await prisma.pokemon.create({ data: { id, name, height, weight, sprite_front_default } });
+    let pokemon = await prisma.pokemon.findUnique({ where: { id } });
+    if (!pokemon) 
+        pokemon = await prisma.pokemon.create({ data: { id, name, height, weight, sprite_front_default } });
+    await prisma.userPokemon.create({ data: { userId, pokemonId: pokemon.id } });
     return pokemon;
 }
 
 export async function releasePokemon(id: number, userId: number) {
-    const pokemon = await prisma.pokemon.delete({ where: { id } });
+    const pokemon = await prisma.userPokemon.delete({ where: { userId_pokemonId: { userId, pokemonId: id } } });
     return pokemon;
 }
 
 export async function getPokemons(userId: number) {
-    const pokemon = await prisma.pokemon.findMany({ where: { id: userId } });
+    const pokemon = await prisma.userPokemon.findMany({ 
+        where: { userId },
+        include: {
+            pokemon: true
+        }
+    });
     return pokemon;
 }
 
 export async function getPokemon(id: number, userId: number) {
-    const pokemon = await prisma.pokemon.findUnique({ where: { id } });
-    return pokemon;
+    const find = await prisma.userPokemon.findUnique({ 
+        where: { userId_pokemonId: { userId, pokemonId: id } },
+        include: {
+            pokemon: true
+        }
+    });
+    if (!find) return null;
+    return find.pokemon;
 }
